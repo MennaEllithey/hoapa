547e9d5b6f102c593ae2fd8f96e79042cfb573f3
Junaid Anwar <junaid4567@gmail.com>
2016-08-16 13:17:44 +0500

    Clean javscript file and optimize code a bit

--- public/js/menna.js
+++ public/js/menna.js
@@ -1,24 +1,25 @@
 $( document ).ready(function() {
-        $(".nav-tabs a").click(function(){window.location.hash =  $(this).attr("href") +'x';});
-       
-        $(window).scroll(function () {
-            if (window.innerWidth > 767) {
-                if ($(window).scrollTop() > 600) {
-                    $("#showOnScroll").addClass('quotes-block');
-                } else {
-                    $("#showOnScroll").removeClass('quotes-block');
-                }
+
+    // Code for making tabs active based on the url
+    $(".nav-tabs a").click(function(){window.location.hash =  $(this).attr("href") +'x';});
+
+    $(window).scroll(function () {
+        if (window.innerWidth > 767) {
+            if ($(window).scrollTop() > 600) {
+                $("#showOnScroll").addClass('quotes-block');
+            } else {
+                $("#showOnScroll").removeClass('quotes-block');
             }
-        });
-        if (location.hash) {
-            currentTab = window.location.hash;
-            currentTab = currentTab.replace("x","")
-            $(".nav-tabs a[href='" + currentTab+"']").click();
         }
     });
+    if (location.hash) {
+        currentTab = window.location.hash;
+        currentTab = currentTab.replace("x","")
+        $(".nav-tabs a[href='" + currentTab+"']").click();
+    }
 
-if( $("#dialog-work-profile-activated").length){
-$(window).load(function(){
-                    $("#dialog-work-profile-activated").modal("show");
-                })
-}
+    // Activate popup right after jquery has loaded we can't wait for the window to load everything
+    if( $("#dialog-work-profile-activated").length){
+        $("#dialog-work-profile-activated").modal("show");
+    }
+});
